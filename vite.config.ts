54f0018e125683c8cfd31b9bc43100fc799c06e4
TD-Yofun <yongfeng.he@china.talkdesk.com>
2024-05-16 13:27:01 +0800

    chore: initiate

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
-import { fileURLToPath, URL } from "node:url";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-import { defineConfig, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config/
-export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return defineConfig({
+export default defineConfig(({ mode }) => {
+  return {
+    mode,
+    plugins: [react()],
     build: {
-      outDir: "dist",
+      outDir: 'dist',
     },
-    plugins: [vue()],
-    define: {
-      "process.env": env,
-    },
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-  });
-};
+  };
+});
