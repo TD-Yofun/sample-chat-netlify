97ad125f4198c7904cc45c468711793ff5ef8421
TD-Yofun <yongfeng.he@china.talkdesk.com>
2024-05-22 17:52:44 +0800

    feat: add vite plugin visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { join } from 'path';
 
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
   return {
     mode,
-    plugins: [react()],
+    plugins: [react(), visualizer()],
     build: {
       outDir: 'dist',
     },
